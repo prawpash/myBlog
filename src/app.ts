b0172d4bda9db923b0a96f0f441a2562feef1527
prawira <robbyprawira3@gmail.com>
2023-04-09 11:47:07 +0700

    👔 Update Main Script
    Set loggin system to true and get data from .env file

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import Fastify from "fastify";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+  production: true,
+};
 const fastify = Fastify({
-  logger: true,
+  logger: envToLogger[process.env.APP_ENV] ?? true,
 });
 
 fastify.get("/test", (request, reply) => {
   return reply.send({ message: "Hello" });
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+fastify.listen(
+  { host: process.env.APP_HOST, port: process.env.APP_PORT },
+  (err, address) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
 
-  fastify.log.info(`Server running on ${address}`);
-});
+    fastify.log.info(`Server running on ${address}`);
+  }
+);
