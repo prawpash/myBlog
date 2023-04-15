269f053190d42ac07b69ce456164d023a8595e94
prawira <robbyprawira3@gmail.com>
2023-04-15 14:08:38 +0700

    🐛 Fix Bug Id Parameter
    Fix bug application not returning error when parameter id just empty
    string

--- src/modules/global/global.schema.ts
+++ src/modules/global/global.schema.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const idSchema = z.object({
-  id: z.string().transform((val) => parseInt(val)),
+  id: z
+    .string()
+    .nonempty()
+    .transform((val) => parseInt(val)),
 });
 
 export type idInput = z.infer<typeof idSchema>;
