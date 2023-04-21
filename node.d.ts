5aa4478a661b4682665cd62dd175066578bd9b64
prawira <robbyprawira3@gmail.com>
2023-04-21 22:25:50 +0700

    🏷️ Update ENV Type

--- node.d.ts
+++ node.d.ts
@@ -1,7 +1,17 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    APP_PORT: number;
-    APP_ENV: "development" | "production";
-    APP_HOST: string;
+import { z } from "zod";
+
+const envSchema = z.object({
+  APP_PORT: z.number(),
+  APP_ENV: z.enum(["development", "production"]),
+  APP_HOST: z.string(),
+  PRIVATE_KEY: z.string(),
+  PUBLIC_KEY: z.string(),
+});
+
+envSchema.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> {}
   }
 }
