5e34e4c2d1a6c19d083b08caf29d62a2f9c1fb91
prawira <robbyprawira3@gmail.com>
2023-04-25 21:37:04 +0700

    👔 Update Route & Controller User
    Add route and function to manage login process

--- src/modules/user/user.controllers.ts
+++ src/modules/user/user.controllers.ts
@@ -1,6 +1,9 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { idInput } from "../global/global.schema";
-import { getUserById } from "./user.services";
+import { findUserByEmail, getUserById } from "./user.services";
+import { loginInput } from "./user.schemas";
+import { verifyPassword } from "../../utils/hash";
+import { generateRandomId } from "../../utils/helper";
 
 export const getUserByIdHandler = async (
   request: FastifyRequest<{
@@ -15,4 +18,49 @@ export const getUserByIdHandler = async (
   if (!data) reply.code(404).send({ message: "User Data Not Found" });
 
   return reply.send(data);
+};
+
+export const loginHandler = async (
+  request: FastifyRequest<{
+    Body: loginInput;
+  }>,
+  reply: FastifyReply
+) => {
+  const body = request.body;
+
+  const user = await findUserByEmail(body.email);
+
+  if (!user)
+    return reply.code(401).send({ message: "Email or Password is incorrect" });
+
+  const isPasswordMatch = await verifyPassword(body.password, user.password);
+
+  if (!isPasswordMatch)
+    return reply.code(401).send({ message: "Email or Password is incorrect" });
+
+  const { id, name } = user;
+
+  const access_token = await reply.jwtSign({ id, name });
+
+  const refresh_token_content = await generateRandomId();
+
+  const refresh_token = await reply.jwtSign(
+    {
+      content: `${refresh_token_content}${user.id}`,
+    },
+    { expiresIn: process.env.APP_REFRESH_TOKEN_LIFETIME }
+  );
+
+  const cookieExpires = new Date(Date.now() + 15 * 60000); // 15 menit
+
+  return reply
+    .setCookie(process.env.APP_COOKIE_TOKEN_NAME, access_token, {
+      domain: process.env.APP_HOST,
+      path: "/",
+      secure: process.env.APP_ENV == "production" ? true : false,
+      httpOnly: true,
+      sameSite: true,
+      expires: cookieExpires,
+    })
+    .send({ access_token, refresh_token });
 };
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,25 @@
 import { FastifyInstance } from "fastify";
-import { getUserByIdHandler } from "./user.controllers";
-import { createUserResponseSchema } from "./user.schemas";
+import { getUserByIdHandler, loginHandler } from "./user.controllers";
+import {
+  createUserResponseSchema,
+  loginResponseSchema,
+  loginSchema,
+} from "./user.schemas";
 import { idSchema } from "../global/global.schema";
 
 const userRoutes = async (fastify: FastifyInstance) => {
+  fastify.post(
+    "/login",
+    {
+      schema: {
+        body: loginSchema,
+        response: {
+          200: loginResponseSchema,
+        },
+      },
+    },
+    loginHandler
+  );
   fastify.get(
     "/:id",
     {
