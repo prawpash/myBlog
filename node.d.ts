48164cf5bce6e6fec241bbb91c1c9169e859e09d
prawira <robbyprawira3@gmail.com>
2023-04-25 21:38:34 +0700

    🏷️ Update ENV Type

--- node.d.ts
+++ node.d.ts
@@ -6,12 +6,15 @@ const envSchema = z.object({
   APP_HOST: z.string(),
   PRIVATE_KEY: z.string(),
   PUBLIC_KEY: z.string(),
+  APP_TOKEN_LIFETIME: z.string(),
+  APP_REFRESH_TOKEN_LIFETIME: z.string(),
+  APP_COOKIE_TOKEN_NAME: z.string(),
 });
 
 envSchema.parse(process.env);
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof envSchema> {}
+    interface ProcessEnv extends z.infer<typeof envSchema> { }
   }
 }
