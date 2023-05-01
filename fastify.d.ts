5e73eaf4c020db42bbc8594b0acf271512c12b57
prawira <robbyprawira3@gmail.com>
2023-05-01 21:35:25 +0700

    🏷️ Update Fastify Type

--- fastify.d.ts
+++ fastify.d.ts
@@ -1,7 +1,17 @@
 import Fastify from "fastify";
+import "@fastify/jwt";
 
 declare module "fastify" {
   interface FastifyInstance {
     authenticate: any;
+  }
+}
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    user: {
+      id: number;
+      name: string;
+    };
   }
 }
