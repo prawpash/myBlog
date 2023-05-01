a6363667484463070d097c51fc95bf4791641f9c
prawira <robbyprawira3@gmail.com>
2023-05-01 21:39:21 +0700

    👔 Create Post Service
    Add function to save post data, update post data, delete post data and
    search post data

---
+++ src/modules/post/post.services.ts
@@ -0,0 +1,116 @@
+import { Status } from "@prisma/client";
+import prisma from "../../utils/prisma";
+import { createPostInput } from "./post.schemas";
+
+export const createPost = async (data: createPostInput, userId: number) => {
+  const categories: Array<{
+    where: { name: string };
+    create: { name: string };
+  }> = [];
+
+  data.categories.forEach((category) => {
+    categories.push({
+      where: { name: category.name },
+      create: { name: category.name },
+    });
+  });
+
+  const post = await prisma.post.create({
+    data: {
+      title: data.title,
+      slug: data.slug,
+      thumbnail: data.thumbnail,
+      content: data.content,
+      owner_id: userId,
+      linked_images: data.linked_images ?? "",
+      status: data.status == "DRAFT" ? Status.DRAFT : Status.PUBLISH,
+      categories: {
+        connectOrCreate: categories,
+      },
+    },
+    include: {
+      categories: true,
+    },
+  });
+
+  return post;
+};
+
+export const updatePost = async (data: createPostInput, id: number) => {
+  const categories: Array<{
+    where: { name: string };
+    create: { name: string };
+  }> = [];
+
+  data.categories.forEach((category) => {
+    categories.push({
+      where: { name: category.name },
+      create: { name: category.name },
+    });
+  });
+
+  const post = await prisma.post.update({
+    where: {
+      id,
+    },
+    data: {
+      title: data.title,
+      slug: data.slug,
+      thumbnail: data.thumbnail,
+      content: data.content,
+      linked_images: data.linked_images ?? "",
+      status: data.status == "DRAFT" ? Status.DRAFT : Status.PUBLISH,
+      categories: {
+        connectOrCreate: categories,
+      },
+    },
+    include: {
+      categories: true,
+    },
+  });
+
+  return post;
+};
+
+export const deletePost = async (id: number) => {
+  const post = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+
+  return post;
+};
+
+export const findPostByQuery = async ({
+  title,
+  categories,
+}: {
+  title?: string;
+  categories?: Array<{ id: number }>;
+}) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      ...(title && { title: { contains: title } }),
+      ...(categories && { categories: { some: { AND: categories } } }),
+    },
+  });
+
+  return posts;
+};
+
+export const findPostBySlug = async (slug: string) => {
+  return await prisma.post.findFirst({
+    where: {
+      slug,
+    },
+  });
+};
+
+export const findPostById = async (id: number) => {
+  return await prisma.post.findUnique({
+    where: {
+      id,
+    },
+  });
+};
